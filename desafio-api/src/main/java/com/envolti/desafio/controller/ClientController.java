0bac80d041bf8c4e734053a1929834c583ea5470
Fernando Medeiros <fernando.medeiros@projuris.com.br>
2021-01-20 13:37:04 -0300

    Alterado o @CrossOrigin para permitir apenas chamadas de localhost:4200

--- desafio-api/src/main/java/com/envolti/desafio/controller/ClientController.java
+++ desafio-api/src/main/java/com/envolti/desafio/controller/ClientController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.envolti.desafio.model.Client;
 import com.envolti.desafio.service.ClientService;
 
+@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/desafio")
 public class ClientController {
