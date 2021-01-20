f8522e455db44a92690484686c6b1fffd2fd5eeb
Fernando Medeiros <fernando.medeiros@projuris.com.br>
2021-01-20 10:07:38 -0300

    Primeira versao.

--- desafio-ui/src/app/app.component.ts
+++ desafio-ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'desafio-ui';
+  @ViewChild('f', { static: false })
+  signupForm!: NgForm;
+  resposta = '';
+  generos = ['FEMININO', 'MASCULINO', 'OUTROS'];
+  user = {
+    dataNascimento: '',
+    nacionalidade: '',
+    naturalidade: '',
+    nome: '',
+    email: '',
+    respostas: '',
+    resposta: '',
+    sexo: ''
+  };
+  submitted = false;
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    this.submitted = true;
+    this.user.dataNascimento = this.signupForm.value.userData.dataNascimento;
+    this.user.nacionalidade = this.signupForm.value.userData.nacionalidade;
+    this.user.naturalidade = this.signupForm.value.userData.naturalidade;
+    this.user.nome = this.signupForm.value.userData.nome;
+    this.user.sexo = this.signupForm.value.userData.sexo;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.resposta = this.signupForm.value.respostas;
+
+    this.signupForm.reset();
+  }
 }
