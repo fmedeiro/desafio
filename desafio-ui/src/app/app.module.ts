0f3758999277d315ba6ecdf5aa88c867b363fbcb
Fernando Medeiros <fernando.medeiros@projuris.com.br>
2021-01-20 13:39:50 -0300

    Alterado para resolver problema de CORS no Angular

--- desafio-ui/src/app/app.module.ts
+++ desafio-ui/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { NgxMaskModule, IConfig } from 'ngx-mask'
 
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     NgxMaskModule.forRoot(),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
